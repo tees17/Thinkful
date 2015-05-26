f44a4ca410f266da8df344c1aa4dbf74ad9ab2c3
tees17 <teresa.o.sears@gmail.com>
2015-05-26 17:40:08 -0500

    First commit for FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,13 @@
+#committing a change for the sake of committing a change
 for i in range (1,100):
 	if i % 3 == 0 and i % 5 == 0:
 		i=str(i)
 		print(i+"FizzBuzz")
 	elif i % 3 == 0:
 		i=str(i)
-		print(i+"Fizz")
+		print(i+"Fizzy")
 	elif i % 5 == 0:
 		i=str(i)
-		print(i+"Buzz")
+		print(i+"Buzzy")
 	else:
 		print(i)
